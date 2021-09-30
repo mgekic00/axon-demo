e783c200644241cba021e6383527bc72c42ed919
Željko Margeta <zeljko.margeta@gmail.com>
2021-09-30 12:12:14 +0200

    feat: setup basic Axon Server Enterprise environment

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-
 ### Build all modules:
 
 `mvn clean package -DskipTests=true`
 
-### Start infrastructure modules in docker:
+### Start environment modules in docker:
 
 `docker-compose up --build`
 
-Rest endpoints: http://localhost:{port}/api/v1/{url}
+### Running Axon Server Enterprise
+
+Please copy axoniq.license to ./config.
--- fake-ledger/src/main/java/com/coinme/hackathon/ledger/FakeLedgerApplication.java
+++ fake-ledger/src/main/java/com/coinme/hackathon/ledger/FakeLedgerApplication.java
@@ -1,12 +1,11 @@
-package com.axondemo.consumer;
+package com.coinme.hackathon.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FakeLedgerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(FakeLedgerApplication.class, args);
-
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FakeLedgerApplication.class, args);
+  }
 }
