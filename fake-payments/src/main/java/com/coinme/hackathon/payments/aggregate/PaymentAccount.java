046cadb0f615dca1859528edf096c1c0c19e28c4
Marko Gekic <marko.gekic@coinme.com>
2021-09-30 20:18:38 +0200

    added commands, events, aggregates

--- fake-payments/src/main/java/com/coinme/hackathon/payments/aggregate/PaymentAccount.java
+++ fake-payments/src/main/java/com/coinme/hackathon/payments/aggregate/PaymentAccount.java
@@ -1,6 +1,13 @@
 package com.coinme.hackathon.payments.aggregate;
 
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+import com.coinme.hackathon.payments.command.CreatePaymentAccountCommand;
+import com.coinme.hackathon.payments.event.PaymentAccountCreatedEvent;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateMember;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -14,4 +21,23 @@ public class PaymentAccount {
   private List<PaymentMethod> paymentMethods;
 
   public PaymentAccount() {}
+
+  @CommandHandler
+  public PaymentAccount (CreatePaymentAccountCommand command) {
+
+    System.out.println("Received "+command.getClass().toString());
+
+    PaymentAccountCreatedEvent event = new PaymentAccountCreatedEvent();
+    event.setUserId(command.getUserId());
+    event.setCreatedAt(LocalDateTime.now().toString());
+
+    apply(event);
+  }
+
+
+  @EventSourcingHandler
+  public void on(PaymentAccountCreatedEvent event) {
+    this.userId = event.getUserId();
+  }
+
 }
--- fake-payments/src/main/java/com/coinme/hackathon/payments/event/PaymentAccountCreatedEvent.java
+++ fake-payments/src/main/java/com/coinme/hackathon/payments/event/PaymentAccountCreatedEvent.java
@@ -2,4 +2,22 @@ package com.coinme.hackathon.payments.event;
 
 public class PaymentAccountCreatedEvent {
 
+  public String userId;
+  public String createdAt;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(String createdAt) {
+    this.createdAt = createdAt;
+  }
 }
